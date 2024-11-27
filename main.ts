61efec21643b1fdebce3307ae6e3abe3988e6572
WeiYuemin <weiyuemin@users.noreply.github.com>
2024-11-27 17:16:36 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-Deno.serve(() => {
-  return fetch(new URL("./Readme.md", import.meta.url));
+
+const OPENAI_API_HOST = "api.openai.com";
+
+Deno.serve(async (request) => {
+  const url = new URL(request.url);
+  url.host = OPENAI_API_HOST;
+
+  const newRequest = new Request(url.toString(), {
+    headers: request.headers,
+    method: request.method,
+    body: request.body,
+    redirect: "follow",
+  });
+  return await fetch(newRequest);
 });
