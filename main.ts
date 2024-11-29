3dc2c79453cc2cdf1a1f87ac7fd13701cd57060d
WeiYuemin <weiyuemin@users.noreply.github.com>
2024-11-29 17:01:46 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 const OPENAI_API_HOST = "api.deepseek.com";
 
+console.log('hello');
+
 Deno.serve(async (request) => {
   const url = new URL(request.url);
   url.host = OPENAI_API_HOST;
+
+  console.log(request.body);
 
   const newRequest = new Request(url.toString(), {
     headers: request.headers,
